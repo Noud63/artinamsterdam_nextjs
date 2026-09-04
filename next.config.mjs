fee0358092acd8c4a31f3410051e1782b9f9eab4
Noud63 <noudvandun@gmail.com>
2026-09-04 07:22:04 +0200

    Disable web vitals attribution

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    webVitalsAttribution: [],
+  },
   images: {
-    remotePatterns: [new URL('https://lh3.googleusercontent.com/a/ACg8ocJvL33LsFmEJ5v3LqSaS_aVWgPZ6c2ZNGrXUNwqZsvtmbNHAU9P=s96-c'), {
+    remotePatterns: [
+      new URL(
+        "https://lh3.googleusercontent.com/a/ACg8ocJvL33LsFmEJ5v3LqSaS_aVWgPZ6c2ZNGrXUNwqZsvtmbNHAU9P=s96-c",
+      ),
+      {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-      }],
+      },
+    ],
   },
 };
 
